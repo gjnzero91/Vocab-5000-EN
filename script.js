b7c2dc854030b0a3f5be546fd474131ea996836a
gjnzero91 <gjnzero.studio@gmail.com>
2025-07-25 07:09:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyDHAr8A7pV5VG8vlT-JyMu0sX6PoX9VAfY",
   authDomain: "vocab-5000-en-13cda.firebaseapp.com",
   projectId: "vocab-5000-en-13cda",
+  storageBucket: "vocab-5000-en-13cda.firebasestorage.app",
+  messagingSenderId: "354480151513",
+  appId: "1:354480151513:web:3c52157c1b071f237417c8",
+  measurementId: "G-S471NR63TR"
 };
 firebase.initializeApp(firebaseConfig);
 
